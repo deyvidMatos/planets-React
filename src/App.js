e6b035a607b199654771965f1a3e4004b453db67
deyvid matos <deyvidmatosdev@gmail.com>
2022-11-08 20:26:08 -0300

    add modal home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Fragment } from 'react';
 import { Outlet } from 'react-router-dom';
 import './App.css';
+import HomeModal from './components/toHome'
 import Planets from './components/planets'
 
 function App() {
   return (
     <div>
       <Outlet/>
+      <HomeModal/>
     </div>
   );
 }
